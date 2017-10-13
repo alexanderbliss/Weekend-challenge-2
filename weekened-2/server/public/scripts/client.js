7a8fc6fa3041662f4ed200cba67eda0e4bf45f53
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-13 16:46:45 -0500

    added and tested buttons

--- weekened-2/server/public/scripts/client.js
+++ weekened-2/server/public/scripts/client.js
@@ -2,4 +2,29 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('script  and jquery sourced')
+    clicks();
+}
+
+function clicks(){
+$('.buttons').on('click', '#add', add );
+$('.buttons').on('click', '#subtract', subtract );
+$('.buttons').on('click', '#multiply', multiply );
+$('.buttons').on('click', '#divide', divide );
+$('.buttons').on('click', '#clear', clear );
+}
+
+function add(){
+console.log('add');
+}
+function subtract() {
+console.log('subtract');
+}
+function multiply() {
+console.log('multiply');
+}
+function divide() {
+console.log('divide');
+}
+function clear() {
+console.log('clear');
 }
